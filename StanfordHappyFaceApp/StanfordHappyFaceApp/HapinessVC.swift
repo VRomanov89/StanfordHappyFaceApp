eb37782a0a23391e4e24e6c64a7ffe7c9adb87d8
Vladimir Romanov <vladromweb@gmail.com>
2016-03-27 21:37:11 -0400

    Finalized Faceview with smile based on const and eyes
    Finalized Faceview with smile based on const and eyes

---
+++ StanfordHappyFaceApp/StanfordHappyFaceApp/HapinessVC.swift
@@ -0,0 +1,13 @@
+//
+//  HapinessVC.swift
+//  StanfordHappyFaceApp
+//
+//  Created by Volodymyr Romanov on 3/27/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+import UIKit
+
+class HapinessVC: UIViewController {
+
+}
